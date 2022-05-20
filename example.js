c17a01d1290dd0831e94d92697a63071be18fe2d
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-05-20 13:54:28 +0530

    feat: add basics of java script example files

---
+++ example.js
@@ -0,0 +1,28 @@
+// let greeting = "hello"
+// let num = 123
+
+// if(num>100){
+//     let greeting = 'hi'
+// }
+
+ test()
+function test() {
+    //const greet;
+    console.log(greet)
+    const greet = "hey hi changed";
+    
+}
+
+// const greet;
+// console.log(greet);
+
+// // var 
+// var v = 123
+// console.log(v)
+// var v = 234
+// console.log(v)
+
+// let v = 123
+// console.log(v)
+// let v = 234
+// console.log(v)
